48ad9097bb91298c4be7d93dbb609ea7381cb577
nisangupta <nishangupta@icloud.com>
2020-10-05 20:11:12 +0545

    working bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +6,16 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+try {
+    window.Popper = require("popper.js").default;
+    window.$ = window.jQuery = require("jquery");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    require("bootstrap");
+} catch (e) {}
+
+window.axios = require("axios");
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,6 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js").sass(
-    "resources/sass/app.sass",
-    "public/css",
-    [
-        //
-    ]
+    "resources/sass/app.scss",
+    "public/css"
 );
