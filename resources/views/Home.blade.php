48285687fad1f673486a79a2e5507477e16c0e10
nisangupta <nishangupta@icloud.com>
2020-10-05 09:59:37 +0545

    fortify installation

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Home laravel 8</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Laravel 8 is the best</h1>
+  </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
